952c1b2aceb0fa05422130175d4c48afb160a17e
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-11 21:45:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,10 @@ class Song
     @name = name
     
     @artist = artist
-    @@artists << artist unless @@artists.include?(artist)
+    @@artists << artist
     
     @genre = genre
-    @@genres << genre unless @@genres.include?(genre)
+    @@genres << genre
   end
   
   def self.count
